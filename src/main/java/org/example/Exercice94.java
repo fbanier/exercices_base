63763745446e26bf49bfc3c6b0c5edf7f31898a7
fbanier <frbanier@gmail.com>
2025-05-13 13:52:29 +0200

    Exercice94

--- src/main/java/org/example/Exercice94.java
+++ src/main/java/org/example/Exercice94.java
@@ -15,11 +15,16 @@ public class Exercice94 {
             centElem[i] = i;
         }
 
-        System.out.println(Arrays.toString(centElem));
         String result = "";
         
         // 2.Afficher les éléments par dizaine séparés d'une virgule
-
+        for (int nbr : centElem){
+            if(nbr == 0) result += nbr + ", ";
+            else{
+                if(nbr%10 == 0) result += "\n" + nbr + ", ";
+                else result += nbr + ", ";
+            }
+        }
 
         System.out.println(result);
         
